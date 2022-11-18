aac6bf527d6cdd8737cd8397e70e79d16aa4d97b
Marlin1812 <stasvakhmintsev@gmail.com>
2022-11-18 10:09:32 +0200

    New2

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition(){
+
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/");
+
+    }
+
+    @Test
+    public void testName(){
+        System.out.println("test started");
+
+    }
+
+    @Test
+    public void testLogin(){
+        System.out.println("test completed");
+
+    }
+
+    @AfterMethod
+    public void postCondition(){
+        // wd.close();
+        wd.quit();
+
+    }
+}
---
+++ src/test/java/com/telran/Main.java
@@ -0,0 +1,8 @@
+package com.telran;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello QA-17!");
+    }
+}
