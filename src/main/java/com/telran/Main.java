9b3ea219023de2b29d4cf680de9874dd9d2203f9
Marlin1812 <stasvakhmintsev@gmail.com>
2022-11-18 12:34:36 +0200

    New3

--- src/main/java/com/telran/Main.java
+++ src/main/java/com/telran/Main.java
@@ -2,11 +2,9 @@ package com.telran;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< Updated upstream
+
         System.out.println("Hello world!");
         System.out.println("Hello QA-17!");
-=======
-        System.out.println("Hello QA-17");
->>>>>>> Stashed changes
+
     }
 }
--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartSelenium {
 
@@ -18,6 +22,14 @@ public class StartSelenium {
     @Test
     public void testName(){
         System.out.println("test started");
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("div"));
+        System.out.println(elements.size());
+        wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.partialLinkText("HO"));
+        wd.findElement(By.id("root"));
+        
+
 
     }
 
@@ -31,7 +43,7 @@ public class StartSelenium {
     public void postCondition(){
 
         // wd.close();
-        wd.quit();
+        //wd.quit();
 
     }
 }
